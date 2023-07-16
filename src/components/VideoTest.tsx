3ce2b1277f2f2b98f9ee581e462b9f858d444441
Vithor Vilas Boas <vithorvilas12@gmail.com>
2023-07-16 14:05:55 -0300

    image prototype and styled hero section

--- src/components/VideoTest.tsx
+++ src/components/VideoTest.tsx
@@ -1,14 +1,10 @@
+import videoRef from "../assets/referenceVideo.mp4";
+import "./VideoTest.css";
+
 export function VideoTest() {
   return (
-    <video autoPlay loop>
-      {/* <source
-        src="https://uploads-ssl.webflow.com/6475c270c3f3fd0a9eb15db5/6490476fe0b6bd35b103ad03_Footer Motion-transcode.mp4"
-        type="video/mp4"
-      />
-      <source
-        src="https://uploads-ssl.webflow.com/6475c270c3f3fd0a9eb15db5/6490476fe0b6bd35b103ad03_Footer Motion-transcode.webm"
-        type="video/mp4"
-      /> */}
+    <video className="VideoTag" autoPlay loop muted>
+      <source src={videoRef} type="video/mp4" />
     </video>
   );
 }
